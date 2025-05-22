a4f55bd860559f7bcd617671ad6020063ec45601
Imran28K <imran28may@gmail.com>
2025-05-22 23:28:05 +0800

    Changes

--- public/login.php
+++ public/login.php
@@ -1,40 +1,36 @@
 <?php
-include 'actions/signup_backend.php';
+include '../actions/login_backend.php';
 ?>
 
 <!DOCTYPE html>
 <html lang="en">
 <head>
     <meta charset="UTF-8">
-    <title>Signup</title>
-    <link rel="stylesheet" href="assets/css/login.css"> <!-- Same CSS as login -->
+    <title>Login</title>
+    <link rel="stylesheet" href="../assets/css/login.css">
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.4/css/all.min.css">
     <link href="https://fonts.googleapis.com/css?family=Raleway:400,700" rel="stylesheet">
 </head>
 <body>
 
 <?php
-include 'includes/navbar.php'; // Navbar same like login
+include '../includes/navbar.php'
 ?>
 
 <div class="container">
     <div class="screen">
         <div class="screen__content">
-            <form class="login" method="POST" action="signup.php">
+            <form class="login" method="POST" action="login.php">
                 <div class="login__field">
                     <i class="login__icon fas fa-user"></i>
-                    <input type="text" name="username" class="login__input" placeholder="Username" required>
-                </div>
-                <div class="login__field">
-                    <i class="login__icon fas fa-envelope"></i>
-                    <input type="email" name="email" class="login__input" placeholder="Email" required>
+                    <input type="text" name="username" class="login__input" placeholder="User name / Email" required>
                 </div>
                 <div class="login__field">
                     <i class="login__icon fas fa-lock"></i>
                     <input type="password" name="password" class="login__input" placeholder="Password" required>
                 </div>
                 <button class="button login__submit" type="submit">
-                    <span class="button__text">Sign Up Now</span>
+                    <span class="button__text">Log In Now</span>
                     <i class="button__icon fas fa-chevron-right"></i>
                 </button>
             </form>
@@ -43,13 +39,10 @@ include 'includes/navbar.php'; // Navbar same like login
             if (isset($error)) {
                 echo "<p style='color:red; margin-top:10px; text-align:center;'>$error</p>";
             }
-            if (isset($success)) {
-                echo "<p style='color:green; margin-top:10px; text-align:center;'>$success</p>";
-            }
             ?>
 
             <div class="social-login">
-                <h3>Sign up via</h3>
+                <h3>log in via</h3>
                 <div class="social-icons">
                     <a href="#" class="social-login__icon fab fa-instagram"></a>
                     <a href="#" class="social-login__icon fab fa-facebook"></a>
@@ -66,6 +59,6 @@ include 'includes/navbar.php'; // Navbar same like login
         </div>		
     </div>
 </div>
-
 </body>
 </html>
+
